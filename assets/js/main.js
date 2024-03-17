ecf96aba13c48690f817275d5ac93cef8f14cbf6
Manash <manashpratimdas64@gmail.com>
2024-03-17 13:24:20 +0530

    added links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Selecao
-* Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
